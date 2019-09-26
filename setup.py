16874853b80c6a9cb7e79b172216964b2b273998
Adrian <adrian.javaloy@gmail.com>
2019-09-26 15:55:26 +0200

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='mlsuite',
+     version='0.1',
+     scripts=[] ,
+     author="Adrián Javaloy Bornás",
+     author_email="adrian.javaloy@gmail.com",
+     description="A framework to ease Machine Learning development.",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/adrianjav/ml-code-base",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3.7",
+#         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
