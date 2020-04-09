b44e4970482984d89887aa14cbf9f67c735992f4
Adrian <adrian.javaloy@gmail.com>
2020-04-09 11:37:25 +0200

    include command in init

--- mlsuite/__init__.py
+++ mlsuite/__init__.py
@@ -1,4 +1,4 @@
-from .experiments import Arguments, YAMLConfig, read_yaml_click, experiment, CLIConfig
+from .experiments import Arguments, YAMLConfig, read_yaml_click, experiment, CLIConfig, command
 
-__all__ = ['Arguments', 'YAMLConfig', 'read_yaml_click', 'experiment', 'CLIConfig']
+__all__ = ['Arguments', 'YAMLConfig', 'read_yaml_click', 'experiment', 'CLIConfig', 'command']
 
